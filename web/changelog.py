e5db532e404c331a225c77b1840d075df7d22acb
Deploy <deploy@bot>
2026-06-16 09:43:26 +0000

    Синхронизация: актуализация всех изменений сессии (кэш, Tailwind, changelog-тесты, редактирование таргетированных ставок)

--- web/changelog.py
+++ web/changelog.py
@@ -1,6 +1,13 @@
-CURRENT_VERSION = "1.9.0"
+CURRENT_VERSION = "1.10.0"
 
 ENTRIES = [
+    {
+        "version": "1.10.0",
+        "date": "16 июня 2026",
+        "items": [
+            "Add ability to edit and manage motivation rules",
+        ],
+    },
     {
         "version": "1.9.0",
         "date": "16 июня 2026",
